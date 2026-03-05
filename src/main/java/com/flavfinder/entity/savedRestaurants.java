3e7aa1d679f52a5b106eacee8ab43db97b8e5031
Emorris70 <emorris7060@gmail.com>
2026-03-05 14:35:05 -0600

    Completed essential items for savedRestaurant class.

--- src/main/java/com/flavfinder/entity/savedRestaurants.java
+++ src/main/java/com/flavfinder/entity/savedRestaurants.java
@@ -3,6 +3,8 @@ package com.flavfinder.entity;
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.util.Objects;
+
 /**
  * Class represents a saved restaurant info card
  *
@@ -30,10 +32,24 @@ public class savedRestaurants {
     @ManyToOne
     private User user;
 
+    /**
+     * Instantiates a new savedRestaurant.
+     */
     public savedRestaurants() {
 
     }
 
+    /**
+     * Instantiates a new savedRestaurants. Constructor allows
+     * for snapshot of the saved restaurant.
+     *
+     * @param restaurantsId restaurant's id
+     * @param name restaurants name
+     * @param category the category
+     * @param imgUrl Unsplash image url matching that category
+     * @param lat latitude of that restaurant
+     * @param lon longitude of that restaurant
+     */
     public savedRestaurants(String restaurantsId, String name, String category,
                             String imgUrl, double lat, double lon)
     {
@@ -41,7 +57,173 @@ public class savedRestaurants {
         this.restaurantName = name;
         this.category = category;
         this.imageUrl = imgUrl;
+        this.latitude = lat;
+        this.longitude = lon;
+    }
 
+    /**
+     * Gets the id
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
 
+    /**
+     * Sets the id
+     *
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the restaurant id
+     *
+     * @return the restaurant id
+     */
+    public String getRestaurantsId() {
+        return restaurantsId;
+    }
+
+    /**
+     * Sets the restaurants id
+     *
+     * @param restaurantsId the restaurant id to set
+     */
+    public void setRestaurantsId(String restaurantsId) {
+        this.restaurantsId = restaurantsId;
+    }
+
+    /**
+     * Gets the restaurants name
+     *
+     * @return the restaurants name
+     */
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    /**
+     * Sets the restaurants name
+     *
+     * @param restaurantName
+     */
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    /**
+     * Gets the category
+     *
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Sets the category
+     *
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * Gets the image url
+     * @return the image url
+     */
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    /**
+     * Sets the image url
+     * @param imageUrl the image url to set
+     */
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    /**
+     * Gets the latitude
+     * @return the latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Sets the latitude
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Gets the longitude
+     * @return the longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Sets the longitude
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * Gets the user
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the user
+     * @param user user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "savedRestaurants{" +
+                "id=" + id +
+                ", restaurantsId='" + restaurantsId + '\'' +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", category='" + category + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        savedRestaurants that = (savedRestaurants) o;
+        return id == that.id && Double.compare(latitude, that.latitude) == 0
+                && Double.compare(longitude, that.longitude) == 0
+                && Objects.equals(restaurantsId, that.restaurantsId)
+                && Objects.equals(restaurantName, that.restaurantName)
+                && Objects.equals(category, that.category)
+                && Objects.equals(imageUrl, that.imageUrl) && Objects.equals(user, that.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, restaurantsId, restaurantName, category, imageUrl, latitude, longitude, user);
     }
 }
