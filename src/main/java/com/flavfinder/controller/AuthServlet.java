c7f698c85d4189b30bdb837de1c48333a168814d
Emorris70 <emorris7060@gmail.com>
2026-03-13 18:23:02 -0500

    Full enviroment modernization.

--- src/main/java/com/flavfinder/controller/AuthServlet.java
+++ src/main/java/com/flavfinder/controller/AuthServlet.java
@@ -1,11 +1,11 @@
 package com.flavfinder.controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.*;
 
 import com.flavfinder.entity.User;
 import com.flavfinder.persistence.GenericDao;
