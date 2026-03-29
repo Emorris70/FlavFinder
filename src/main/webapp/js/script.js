7f77fbfe335f04b947f752487d09306662be8a0a
Emorris70 <emorris7060@gmail.com>
2026-03-28 21:24:55 -0500

    Starting process of drop down features

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -2,4 +2,10 @@ const init = () => {
 console.log("Hello World!!");
 }
 
+const dropDown = () => {
+    const dropDownDiv = document.getElementById("drop-down");
+    dropDownDiv.addEventListener("click", () => {
+
+    });
+}
 window.addEventListener("DOMContentLoaded", init);
